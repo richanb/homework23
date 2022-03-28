57b37c2261f8fea4cddcad5c0e78929e5ccac92f
rickychandra <87794740+richanb@users.noreply.github.com>
2022-03-28 15:55:12 +0700

    commit

---
+++ src/pages/home/indeks.js
@@ -0,0 +1,11 @@
+import data from "../../data/dummy";
+import Data from "../../components";
+
+function Album() {
+    return (
+       <div>
+           <Data data={data}/>
+       </div>
+    )
+}
+export default Album;
